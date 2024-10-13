050a3ad5dba6e20f6fe4fce59c5c6e2d4b36f761
valentinR08 <valentonriversan@gmail.com>
2024-10-13 17:58:56 -0500

    ultimo cambio de la funcion en javascript

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,12 @@
 const form = document.getElementById('formulario');
+const boton = document.getElementById('boton');
+const contenedor = document.getElementById('mensaje');
+const textArea = document.getElementById('comentario');
+const inputs = document.querySelectorAll('#formulario input');
 
-form.addEventListener('submit', ()=>{
-    
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
     // Desactivar los campos del formulario
-    const inputs = document.querySelectorAll('#formulario input');
-    const textArea = document.getElementById('comentario');
-    const boton = document.getElementById('boton');
     inputs.forEach(input => input.disabled = true);
     textArea.disabled = true;
     boton.disabled = true;
@@ -14,15 +15,17 @@ form.addEventListener('submit', ()=>{
     console.log(nombre);
     // Mostrar el mensaje de registro
      // Crear dinámicamente el elemento <p> y añadir el mensaje
-     const contenedor = document.getElementById('mensaje');
      const mensaje = document.createElement('p');
      mensaje.textContent = `El usuario ${nombre} se registró correctamente.`;
      contenedor.appendChild(mensaje);
     
-
-    // Redirigir a otra ruta después de 3 segundos
+     
+    
     setTimeout(() => {
+        inputs.forEach(input => input.disabled = false);
+        textArea.disabled = false;
+        boton.disabled = false;
         mensaje.remove();
-        
-    }, 3000); // Espera 3 segundos antes de redirigir
+        form.submit();
+    }, 4000); //se espera 4 segundos para borrar el mensaje
 });
